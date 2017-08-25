bee476027adf93b55ee09e9373199eacee25e5d1
Oley-Projects <oley.for.git@gmail.com>
2017-08-25 18:15:22 +0300

    Database init

--- app.rb
+++ app.rb
@@ -3,10 +3,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def get_db
+	db = SQLite3::Database.new 'vlogerok.db'
+	db.results_as_hash = true
+	return db
+end
+
+configure do
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS "Posts" 
+		("id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		 "createddate" DATE,
+		 "details" TEXT ) '
+	db.execute 'CREATE TABLE IF NOT EXISTS "Comments"
+		("id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		 "createddate" DATE,
+		 "comment" TEXT )'
+end
+
 get '/' do
 	erb "Hello!"
 end
 
 get '/new' do
   	erb :new
+end
+
+post '/new' do
+  	@content = params[:content]
+
 end
